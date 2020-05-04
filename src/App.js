c6be94de21688b967d0682e031b93312e4146e7f
Ahmad <hamadagh777@gmail.com>
2020-05-04 17:20:25 +0200

    adding react routes and home, add sections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/home/Home';
+import Add from './components/add/Add';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/Add" component={Add} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/add/Add.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Add = () => {
+    return (
+        <div className="add-section">
+
+        </div>
+    )
+}
+
+export default Add
